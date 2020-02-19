a1728d6404350c58b51856b8a27f13851ee73d0f
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-19 19:21:21 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length == 0 || array.length == 0) {
+        return 0;
+    }
+    let min = array[0];
+    for (let elem of array) {
+        if (elem < min) min = elem;
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length == 0 || array.length == 0) {
+        return 0;
+    }
+    let max = array[0];
+    for (let elem of array) {
+        if (elem > max) max = elem;
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length == 0 || array.length == 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let elem of array) {
+        sum += elem;
+    }
+    return sum / array.length;
 }
